d81568397f44eccc04f75c955724d00464618874
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-08-15 15:59:06 -0300

    Page Container

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
     compiler: {
-        styledComponents: true,
+        styledComponents: true
     },
 }
   
