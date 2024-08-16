7b4e9c48c85d22aabd475c3269b8dc5ab79a6d1f
Otello Boz Maraldo <homero.tw@gmail.com>
2024-08-16 06:26:11 -0300

    Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import PageContainer from "@/components/layout/PageContainer";
-import Header from "@/components/layout/Header";
+import Header from "@/components/layout/header";
 import Main from "@/components/layout/Main";
 import Footer from "@/components/layout/Footer";
 
