5f2a4be05da3014d6a04b5b4d0fa40a1c88f1165
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-09-13 13:47:08 -0300

    Full Sample

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Heading, Flex } from '@/once-ui/components';
+
+const Contact = () => {
+  return (
+    <Flex
+        direction="column" alignItems="center"
+        padding="l" fillWidth>
+        <Heading variant="body-default-l">Contact</Heading>
+    </Flex>
+  );
+};
+
+export default Contact;
---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Heading, Flex } from '@/once-ui/components';
+
+const Portfolio = () => {
+  return (
+    <Flex
+        direction="column" alignItems="center"
+        padding="l" fillWidth>
+        <Heading variant="body-default-l">Portfolio</Heading>
+    </Flex>
+  );
+};
+
+export default Portfolio;
